eca96c12d336ed79ad346fdec202f4e413c050c6
Yasmin Reis Tello <mimirt08@gmail.com>
2022-06-21 10:17:13 -0300

    Correcoes Linter

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -44,7 +44,7 @@ const calculator = (number1, number2) => ({
 
 const arrayGenerator = (type, object) => {
   if (type === 'keys') {
-   return Object.keys(object);
+    return Object.keys(object);
   }
   if (type === 'values') {
     return Object.values(object);
@@ -52,6 +52,6 @@ const arrayGenerator = (type, object) => {
   if (type === 'entries') {
     return Object.entries(object);
   }
-  };
+};
 
 module.exports = { calculator, arrayGenerator };
